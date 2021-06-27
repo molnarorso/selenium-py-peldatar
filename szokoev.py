169f0bf1bb2a62f6f6dbe45fe6ff71c335a171bd
morso <molnarorso@gmail.com>
2021-06-27 22:57:52 +0200

    Feladat: Python függvények gyakorlása

---
+++ szokoev.py
@@ -0,0 +1,32 @@
+# year_input = int(input("Enter a year, and I will tell you if it is a leap year: "))
+# if year_input%4 == 0:
+#     if year_input%100 == 0:
+#         if year_input%400 == 0:
+#             print("It is a leap year")
+#         else:
+#             print("It is not a leap year")
+#     else:
+#         print("It is a leap year")
+# else:
+#     print("It is not a leap year")
+
+year_input = int(input("Enter a year, and I will tell you if it is a leap year: "))
+
+def function(year_input):
+    output = False
+    if year_input%4 == 0:
+        if year_input%100 == 0:
+            if year_input%400 == 0:
+                output = True
+            else:
+                output = False
+        else:
+            output = True
+    else:
+        output = False
+    return output
+
+if function(year_input) == True:
+    print("It is a leap year")
+else:
+    print("It is not a leap year")
---
+++ tenbelow.py
@@ -0,0 +1,12 @@
+list_of_inputs = []
+user_input = 0
+
+while True:
+    user_input = int(input("Írj be egy számot, ami kisebb mint 10: "))
+    if user_input >= 10:
+        break
+    list_of_inputs.append(user_input)
+
+print(sum(list_of_inputs))
+
+

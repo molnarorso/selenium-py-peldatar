00ad89c7122cc9887fa28a2b50c94bf0d7bb28cc
morso <molnarorso@gmail.com>
2021-07-11 08:30:15 +0200

    017 Feladat: Python dictionary gyakorlása

---
+++ selenium2-homework/csvszures.py
@@ -0,0 +1,11 @@
+import csv
+
+with open('fajlcsvszureshez.csv', 'w', newline='', encoding='utf-8') as f0:
+    with open('table_in.csv', 'r', newline='', encoding='utf-8') as f1:
+        reader = csv.reader(f1)
+        for i in reader:
+            f0.write(i[1])
+            f0.write(',')
+            f0.write(i[0])
+            f0.write('\n')
+

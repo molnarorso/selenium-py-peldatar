de64349f8bc7e4d6b6a386c5f49aa62118d34955
morso <molnarorso@gmail.com>
2021-07-11 06:08:45 +0200

    017 Feladat: Python dictionary gyakorlása

---
+++ selenium2-homework/linkek.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+PATH = "C:\\Users\\ors\\Downloads\\chromedriver.exe"
+URL = "http://localhost:9999"
+
+browser = webdriver.Chrome(PATH)
+browser.maximize_window()
+browser.get(URL)
+
+elements = browser.find_elements_by_tag_name('a')
+links = []
+for i in elements:
+    links.append(i.get_attribute('href'))
+
+with open('linkekhezfajl.txt', 'w') as f:
+    for j in links:
+        f.write(j + '\n')
+
+print(len(links))

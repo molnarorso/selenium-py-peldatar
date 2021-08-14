9090ddc06275eb0e95a5a299198e2c244de1a8bd
morso <molnarorso@gmail.com>
2021-08-14 02:46:41 +0200

    selenium-2

--- selenium2-homework/edittable.py
+++ selenium2-homework/edittable.py
@@ -1,8 +1,6 @@
 import time
-
 from selenium import webdriver
 import pyautogui
-import pyperclip
 
 PATH = "C:\\Users\\ors\\Downloads\\chromedriver.exe"
 URL = "http://localhost:9999/editable-table.html"
@@ -22,6 +20,7 @@ def completing_entry(my_data):
     pyautogui.write(my_data)
 
 
+# Adding two new lines
 browser.find_element_by_xpath("//button[normalize-space()='Add']").click()
 browser.implicitly_wait(2)
 browser.find_element_by_xpath("//tbody/tr[7]/td[1]/input[1]").send_keys('Goat cheese')
@@ -31,7 +30,6 @@ tabbing()
 completing_entry('30')
 tabbing()
 completing_entry('Dairy')
-
 browser.find_element_by_xpath("//button[normalize-space()='Add']").click()
 browser.implicitly_wait(2)
 browser.find_element_by_xpath("//tbody/tr[8]/td[1]/input[1]").send_keys('Carrots')
@@ -42,6 +40,7 @@ completing_entry('1000')
 tabbing()
 completing_entry('Vegetables')
 
+# Testing the search function
 time.sleep(0.5)
 browser.find_element_by_xpath("//input[@placeholder='Search...']").click()
 pyautogui.write('ba')
@@ -51,10 +50,26 @@ assert len(search_results1) == 3
 pyautogui.write('s')
 search_results2 = browser.find_elements_by_class_name("eachRow")
 assert len(search_results2) == 2
+pyautogui.hotkey('backspace')
+pyautogui.hotkey('backspace')
+pyautogui.hotkey('backspace')
 
-time.sleep(1)
+# Rewriting data in table
+time.sleep(0.5)
+browser.find_element_by_xpath("//tbody/tr[2]/td[1]/input[1]").clear()
+browser.find_element_by_xpath("//tbody/tr[2]/td[1]/input[1]").send_keys('volleyball')
+tabbing()
+time.sleep(0.5)
+browser.find_element_by_xpath("//tbody/tr[5]/td[1]/input[1]").clear()
+browser.find_element_by_xpath("//tbody/tr[5]/td[1]/input[1]").send_keys('iPhone 9')
+tabbing()
+
+# Asserting changes
+browser.implicitly_wait(0.5)
+item_to_be_asserted1 = browser.find_element_by_xpath("//tbody/tr[2]/td[1]/input[1]")
+assert item_to_be_asserted1.get_attribute("value") == 'volleyball'
+browser.implicitly_wait(0.5)
+item_to_be_asserted2 = browser.find_element_by_xpath("//tbody/tr[5]/td[1]/input[1]")
+assert item_to_be_asserted2.get_attribute("value") == 'iPhone 9'
+
 browser.quit()
-
-
-
-
